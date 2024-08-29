7d2d62967168a7ca8b847c4c1ce19cd4d000e665
mahatohariom <mahatohariom31@gmail.com>
2024-08-29 18:44:39 +0545

    Htpperror is used in the music controller

--- src/controllers/music-controller.ts
+++ src/controllers/music-controller.ts
@@ -1,142 +1,122 @@
 import { Request, Response } from "express";
-
+import { asyncWrapper } from "../utils/async-wrapper";
+import { HttpError } from "../utils/http-error";
 import { Music } from "../types";
 import { musicValidation } from "../validation/music";
 import { client } from "../db";
 
-export const getMusic = async (
-  req: Request,
-  res: Response
-): Promise<Response> => {
-  try {
-    const result = await client.query("SELECT * FROM music");
-    return res.status(200).json({ data: result.rows });
-  } catch (error) {
-    return res.status(500).json({ message: "Internal server error", error });
+export const getMusic = asyncWrapper(async (req: Request, res: Response) => {
+  const result = await client.query("SELECT * FROM music");
+  if (result.rowCount === 0) {
+    throw new HttpError("No music records found", 404);
   }
-};
+  return res.status(200).json({ data: result.rows });
+});
 
-export const getMusicById = async (
-  req: Request,
-  res: Response
-): Promise<Response> => {
-  const { id } = req.params;
-  try {
+export const getMusicById = asyncWrapper(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
     const result = await client.query({
       text: "SELECT * FROM music WHERE id = $1",
       values: [id],
     });
-    if (!result.rows[0]) {
-      return res.status(404).json({ message: "Music not found" });
+    if (result.rowCount === 0) {
+      throw new HttpError("Music not found", 404);
     }
     return res.status(200).json({ data: result.rows[0] });
-  } catch (error) {
-    return res.status(500).json({ message: "Internal server error", error });
   }
-};
+);
 
-export const getMusicByArtistId = async (
-  req: Request,
-  res: Response
-): Promise<Response> => {
-  const { artistId } = req.params;
-  try {
+export const getMusicByArtistId = asyncWrapper(
+  async (req: Request, res: Response) => {
+    const { artistId } = req.params;
     const result = await client.query({
       text: "SELECT * FROM music WHERE artist_id = $1",
       values: [artistId],
     });
-    if (!result.rows[0]) {
-      return res.status(404).json({ message: "Music not found" });
+    if (result.rowCount === 0) {
+      throw new HttpError("Music not found", 404);
     }
     return res.status(200).json({ data: result.rows });
-  } catch (error) {
-    return res.status(500).json({ message: "Internal server error", error });
   }
-};
+);
 
-export const createMusic = async (
-  req: Request,
-  res: Response
-): Promise<Response> => {
+export const createMusic = asyncWrapper(async (req: Request, res: Response) => {
   const { title, album_name, artist_id, genre } = req.body as Music;
-  console.log(title, album_name, artist_id, genre, "S");
   const validationResult = musicValidation(req.body);
   if (!validationResult.success) {
     const errorMessages = validationResult.error.errors.map(
       (err) => err.message
     );
-    return res.status(400).json({ message: errorMessages });
+    throw new HttpError(errorMessages.join(", "), 400);
   }
-  console.log(title, album_name, artist_id, genre, "S");
+
   const query = `
     INSERT INTO music (title, album_name, artist_id, genre)
     VALUES ($1, $2, $3, $4)
     RETURNING *`;
   const values = [title, album_name, artist_id, genre];
 
-  try {
-    const result = await client.query(query, values);
-    return res
-      .status(201)
-      .json({ message: "Music created successfully", data: result.rows[0] });
-  } catch (error) {
-    return res.status(500).json({ message: "Internal server error", error });
+  const result = await client.query(query, values);
+  if (result.rowCount === 0) {
+    throw new HttpError("Failed to create music record", 400);
   }
-};
+  return res
+    .status(201)
+    .json({ message: "Music created successfully", data: result.rows[0] });
+});
 
-export const deleteMusicById = async (
-  req: Request,
-  res: Response
-): Promise<Response> => {
-  const { id } = req.params;
+export const deleteMusicById = asyncWrapper(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
 
-  const musicExists = await client.query({
-    text: "SELECT * FROM music WHERE id = $1",
-    values: [id],
-  });
+    const musicExists = await client.query({
+      text: "SELECT * FROM music WHERE id = $1",
+      values: [id],
+    });
 
-  if (!musicExists.rows[0]) {
-    return res.status(404).json({ message: "Music not found" });
-  }
-  try {
+    if (musicExists.rowCount === 0) {
+      throw new HttpError("Music not found", 404);
+    }
+
     await client.query({
       text: "DELETE FROM music WHERE id = $1 RETURNING *",
       values: [id],
     });
-    return res.status(200).json({ message: "Music deleted" });
-  } catch (error) {
-    return res.status(500).json({ message: "Internal server error", error });
+
+    return res.status(200).json({ message: "Music deleted successfully" });
   }
-};
+);
 
-export const updateMusicById = async (
-  req: Request,
-  res: Response
-): Promise<Response> => {
-  const { id } = req.params;
-  const { title, album_name, artist_id, genre } = req.body as Music;
+export const updateMusicById = asyncWrapper(
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { title, album_name, artist_id, genre } = req.body as Music;
 
-  const musicExists = await client.query({
-    text: "SELECT * FROM music WHERE id = $1",
-    values: [id],
-  });
-  if (!musicExists.rows[0]) {
-    return res.status(404).json({ message: "Music not found" });
-  }
+    const musicExists = await client.query({
+      text: "SELECT * FROM music WHERE id = $1",
+      values: [id],
+    });
 
-  try {
+    if (musicExists.rowCount === 0) {
+      throw new HttpError("Music not found", 404);
+    }
+
     const query = `
-      UPDATE music 
-      SET title = $1, album_name = $2, artist_id = $3, genre = $4
-      WHERE id = $5
-      RETURNING *;`;
+    UPDATE music 
+    SET title = $1, album_name = $2, artist_id = $3, genre = $4
+    WHERE id = $5
+    RETURNING *;`;
     const values = [title, album_name, artist_id, genre, id];
+
     const result = await client.query(query, values);
+
+    if (result.rowCount === 0) {
+      throw new HttpError("Failed to update music record", 400);
+    }
+
     return res
       .status(200)
       .json({ message: "Music updated successfully", data: result.rows[0] });
-  } catch (error) {
-    return res.status(500).json({ message: "Internal server error", error });
   }
-};
-
+);
