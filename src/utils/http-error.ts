b9ee029b3c312fc62290c5f63b21441ef3fb5847
mahatohariom <mahatohariom31@gmail.com>
2024-08-29 18:27:40 +0545

    http error created

--- src/utils/http-error.ts
+++ src/utils/http-error.ts
@@ -1,12 +1,11 @@
+
 export class HttpError extends Error {
   statusCode: number;
 
   constructor(statusCode: number, message: string) {
     super(message);
     this.statusCode = statusCode;
-
-    if (Error.captureStackTrace) {
-      Error.captureStackTrace(this, this.constructor);
-    }
+    // Ensures proper stack trace for where the error was thrown
+    Error.captureStackTrace(this, this.constructor);
   }
 }
