193e813655f52d58974e83deb37cdbf7b869f50b
mahatohariom <mahatohariom31@gmail.com>
2024-09-01 18:01:00 +0545

    server also being added env example

--- src/app.ts
+++ src/app.ts
@@ -6,28 +6,23 @@ import musicRoutes from "./routes/music-route";
 import { errorHandler } from "./middleware/error-handler";
 import cors from "cors";
 import dotenv from "dotenv";
-
-// initialize the dotenv so that all env file can be properly read
 dotenv.config();
-// initialize app here
+
 const app = express();
 
-// use to parse the json object
 app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000", // Replace with your frontend URL
+    origin: process.env.FRONTEND_URL || "http://localhot:3000",
     credentials: true,
   })
 );
 
-// use the  routes here
 app.use("/api/v1/users", userRoutes);
 app.use("/api/v1/artist", artistRoutes);
 app.use("/api/v1/music", musicRoutes);
 
-// we have created a middleware to catch the  error
 app.use(errorHandler);
 
 export default app;
